cc49aceceb69e602ce90cebc0253a46509681dc1
acrojuno <89344894+acrojuno@users.noreply.github.com>
2022-03-23 01:01:52 +0900

    서버로 이미지 업로드 기능 추가
    http를 이용해 서버로 이미지 업로드 하는 기능 추가
    
    아직 테스트 안 해봄

--- FrontEnd_Flutter/maskeraser/lib/main.dart
+++ FrontEnd_Flutter/maskeraser/lib/main.dart
@@ -5,6 +5,47 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:dio/dio.dart';
+import 'package:path/path.dart';
+import 'dart:async';
+
+//http 이용해서 이미지 서버로 업로드 하는 함수
+/**
+ * accepts three parameters, the endpoint, formdata (except fiels),files (key,File)
+ * returns Response from server
+ */
+Future<Response> sendForm(
+    String url, Map<String, dynamic> data, Map<String, XFile?> files) async {
+  Map<String, MultipartFile> fileMap = {};
+  for (MapEntry fileEntry in files.entries) {
+    File file = fileEntry.value;
+    String fileName = basename(file.path);
+    fileMap[fileEntry.key] =
+        MultipartFile(file.openRead(), await file.length(), filename: fileName);
+  }
+  data.addAll(fileMap);
+  var formData = FormData.fromMap(data);
+  Dio dio = new Dio();
+  return await dio.post(url,
+      data: formData, options: Options(contentType: 'multipart/form-data'));
+}
+
+/**
+ * accepts two parameters,the endpoint and the file
+ * returns Response from server
+ */
+Future<Response> sendFile(String url, XFile file) async {
+  Dio dio = new Dio();
+  var len = await file.length();
+  var response = await dio.post(url,
+      data: file.openRead(),
+      options: Options(headers: {
+        Headers.contentLengthHeader: len,
+      } // set content-length
+      ));
+  return response;
+}
+
 
 void main() => runApp(MyApp());
 
@@ -43,6 +84,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   File? _image;
+  Dio dio = new Dio();
+
+
+
 
   /* 카메라로 직접 찍은 사진을 가져와 컨테이너(박스)에 넣는 함수
   Future getImagefromcamera() async {
@@ -58,10 +103,19 @@ class _MyHomePageState extends State<MyHomePage> {
   Future getImagefromGallery() async {
     var image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
+    //upload image
+    //scenario  one - upload image as poart of formdata
+    var res1 = await sendForm('http://192.168.43.236:4082/create-profile',
+        {'name': 'iciruit', 'des': 'description'}, {'profile': image});
+    print("res-1 $res1");
+
     setState(() {
       _image = File(image!.path);
     });
+
   }
+
+
 
   @override
   Widget build(BuildContext context) {
